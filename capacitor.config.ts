ea0fda5d3c861f973cad1d58606e3ea481e2dd21
kokeluis <jo.valdiviav@duocuc.cl>
2023-09-16 22:18:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RegistraAPP',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
